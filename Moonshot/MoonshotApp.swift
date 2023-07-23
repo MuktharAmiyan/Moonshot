c295c92eb102642047ce89c63ea4dedb434a4334
MuktharAmiyan <amiyanmukthar@gmail.com>
2023-07-23 12:40:27 +0530

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Mukthar Amiyan on 23/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
